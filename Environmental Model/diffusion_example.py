dd540263f045ff38a6421935894ee00b9fb5b949
Michael <mrastwoo@edu.uwaterloo.ca>
2019-08-02 15:22:01 -0400

    cleanup and 3d model

--- Environmental Model/diffusion_example.py
+++ Environmental Model/diffusion_example.py
@@ -9,52 +9,44 @@ from fenics import *
 import dolfin as df
 
 import numpy as np
-dt = 0.01
+dt = 0.1
 t=0
 # Create mesh and define function space
-mesh = RectangleMesh(Point(-2,-2),Point(2,2),20, 20)
+mesh = BoxMesh(Point(-2,-2,-2),Point(2,2,2),20,20,20)
 V = FunctionSpace(mesh, 'P',1)
 
-# Define boundary condition
-import sympy as sy
-x,y=sy.symbols('x[0],x[1]')
-
-u_0 = Expression('exp(-10*a*pow(x[0]+2,2))',
-                 degree=2, a=5)
+u_0 = Expression('exp(-10*a*pow(x[2]-2,2))',
+                 degree=3, a=5)
 def boundary(x, on_boundary):
     return on_boundary and abs(x[0]+2)>1e-14
 def boundaryLeft(x,on_boundary):
     return on_boundary and abs(x[0]+2)<=1e-14
-bc1 = DirichletBC(V, Constant(0), boundary)
+def boundaryBottom(x,on_boundary):
+    return on_boundary and abs(x[1]-2)<=1e-14
 
-bc=[bc1]
 Dx=1
-Dy=0.1
+Dy=1
 Dz=1
 # Define variational problem
-D = sym(as_tensor([[Dx, 0],
-	           [0, Dy]])) 
+D = sym(as_tensor([[Dx,  0,  0],
+	           [0,  Dy,  0],
+	           [0,   0, Dz]])) 
 # dispersion coefficient matrix
 uA = Function(V)  # Note: not TrialFunction!
 uB = interpolate(u_0,V)
 v = TestFunction(V)
-tF=100
-
-F = dot(D*grad(uA), grad(v))*dx + v*(uA-uB)/dt*dx
+tF=10
+w=-0.1
+F = dot(D*grad(uA), grad(v))*dx + v*(uA-uB)/dt*dx - v*w*grad(uA)[2]*dx
 import matplotlib.pyplot as pl
 counter = 1
+vtkfile = File('diffusion3d/solution.pvd')
 while t<=tF:
 	t+=dt
 	solve(F==0,uA)
-	
-	vtkfile = File('nonlinheat_testing/solution_'+str(counter)+'.pvd')
-	vtkfile << uA
+	uA.rename('uA','uA')
+	vtkfile << uA, counter
 	uB.assign(uA)
-
-	# Plot solution
-	plot(uB)
-	pl.show()
-	pl.savefig('nonlinheat/nonlin_heat'+str(counter)+'.png')
 	counter+=1
 # Compute maximum error at vertices. This computation illustrates
 # an alternative to using compute_vertex_values as in poisson.py.
