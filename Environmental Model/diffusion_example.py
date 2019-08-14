3be88cf2d7d785ff41f8f105629f703e30064ed7
Francois Lemesre <francoislemesre@gmail.com>
2019-08-13 23:35:13 -0400

    more verbose comments for diffusion_example, formatting fixes

--- Environmental Model/diffusion_example.py
+++ Environmental Model/diffusion_example.py
@@ -2,68 +2,70 @@
 from fenics import *
 import dolfin as df
 import numpy as np
-import matplotlib.pyplot as pl
 import mshr as ms
 
 dt = 0.05
-t=0
+t = 0
+
 
 class Nodule(SubDomain):
-	def inside(self,x,on_boundary):
-		r=[0,0,0]
+	def inside(self, x, on_boundary):
+		r = [0, 0, 0]
 		R = ((x[0]-r[0])**2+(x[1]-r[1])**2+(x[2]-r[2])**2)**0.5
 		return True if R <= 1 else False
 
-#Define subdomains (root nodules) - prototype class
-subdomain1=Nodule() #define a new root nodule
 
-# Create mesh and define function space
-mesh = BoxMesh(Point(-10,-10,-10),Point(10,10,10),30,30,30)
-#mesh=Mesh('geometry.xml')
-V = FunctionSpace(mesh, 'P',1)
-dofmap = V.dofmap() #this basically lets us grab the raw data from the mesh about cells
+# Define subdomains (root nodules) - prototype class:
+subdomain1 = Nodule()  # define a new root nodule
 
-u_0 = Expression('0.001*exp(-1*(pow(x[2]-2,2)+0.1*pow(x[1],2)+0.1*pow(x[0],2)))',
-                 degree=2)
-cf=MeshFunction('size_t',mesh,3) 
-#define a function with values on the cells of the mesh (the 3 means '3d' blocks)
+# Create mesh and define function space:
+mesh = BoxMesh(Point(-10, -10, -10), Point(10, 10, 10), 30, 30, 30)
+# mesh=Mesh('geometry.xml')
+V = FunctionSpace(mesh, 'P', 1)
+dofmap = V.dofmap()  # this basically lets us grab the raw data from the mesh about cells
 
-subdomain1.mark(cf,1) #mark the function as 1 inside the subdomain
-heaviside = Function(V) #define a function in V - this will be the heaviside
-for cell in cells(mesh): # set the characteristic functions
-    if cf[cell] == 1:
-        heaviside.vector()[dofmap.cell_dofs(cell.index())] = 1
-	#project the characteristic function cf into our space V 
-	# ie - this is the heaviside function	
-Dx=Constant(9.2e-7*100)
-Dy=Constant(9.2e-7*100)
-Dz=Constant(2.1e-6*100)
-# Define variational problem
-D = sym(as_tensor([[Dx,  0,  0],
-	           [0,  Dy,  0],
-	           [0,   0, Dz]])) 
-# dispersion coefficient matrix
+u_0 = Expression('0.001*exp(-1*(pow(x[2]-2,2)+0.1*pow(x[1],2)+0.1*pow(x[0],2)))', degree=2)
+cf = MeshFunction('size_t', mesh, 3)  # define a function with values on the cells of the mesh (the 3 means '3d' blocks)
+
+subdomain1.mark(cf, 1)  # mark the function as 1 inside the subdomain
+heaviside = Function(V)  # define a function in V - this will be the heaviside
+for cell in cells(mesh):  # set the characteristic functions
+	if cf[cell] == 1:
+		heaviside.vector()[dofmap.cell_dofs(cell.index())] = 1
+		# project the characteristic function cf into our space V
+		# ie - this is the heaviside function
+
+# Defining the variational problem:
+Dx = Constant(9.2e-7*100)
+Dy = Constant(9.2e-7*100)
+Dz = Constant(2.1e-6*100)
+
+# Dispersion coefficient matrix:
+D = sym(as_tensor([[Dx, 0, 0],
+				   [0, Dy, 0],
+				   [0, 0, Dz]]))
+
 uA = TrialFunction(V)
 u = Function(V)  # Note: not TrialFunction!
 uB = interpolate(u_0,V)
 v = TestFunction(V)
 
-tF=50
-w=Constant(0.004*100)
-K=10000*heaviside #degradation only happening inside the subdomain
+tF = 50
+w = Constant(0.004*100)
+K = 10000*heaviside  # degradation only happening inside the subdomain
 A = inner(D*grad(uA), grad(v))*dt*dx + v*uA*dx - v*w*dt*grad(uA)[2]*dx + dt*K*uA*v*dx
-L=v*uB*dx
+L = v*uB*dx
 counter = 1
 vtkfile = File('diffusion3d_2/solution.pvd')
 
-while t<=tF:
-	t+=dt
-	solve(A==L,u)
-	u.rename('u','u')
+while t <= tF:
+	t += dt
+	solve(A == L, u)
+	u.rename('u', 'u')
 	if counter % 1 == 0:
 		vtkfile << u, counter
 		print('\nTimestep: '+str(counter)+'\n')
 	uB.assign(u)
-	counter+=1
+	counter += 1
 
 
