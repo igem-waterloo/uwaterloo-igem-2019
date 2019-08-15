b427e83d9099cd28e9cbe224d2cbd9c283467cd4
Michael <mrastwoo@edu.uwaterloo.ca>
2019-08-15 17:01:51 -0400

    some files

--- Environmental Model/diffusion_prod.py
+++ Environmental Model/diffusion_prod.py
@@ -18,14 +18,13 @@ class Nodule(SubDomain):
 subdomain1=Nodule() #define a new root nodule
 
 # Create mesh and define function space
-##mesh = BoxMesh(Point(-10,-10,-10),Point(10,10,2),30,30,30)
-#we need to read mesh from xml file
-mesh=Mesh('geometry.xml')
+mesh = BoxMesh(Point(-10,-10,-10),Point(10,10,10),30,30,30)
+#mesh=Mesh('geometry.xml')
 V = FunctionSpace(mesh, 'P',1)
 dofmap = V.dofmap() #this basically lets us grab the raw data from the mesh about cells
 
-u_0 = Expression('10*exp(-a*(pow(x[1],2)+pow(x[0],2)+pow(x[2]-1.5,2)))',
-                 degree=3, a=0.1)
+u_0 = Expression('(0.005/pow(25*2*3.141,0.5))*exp(-1*(0.5*pow(x[2]-2,2)+0.1*pow(x[1],2)+0.1*pow(x[0],2)))',
+                 degree=2)
 cf=MeshFunction('size_t',mesh,3) 
 #define a function with values on the cells of the mesh (the 3 means '3d' blocks)
 
@@ -36,31 +35,35 @@ for cell in cells(mesh): # set the characteristic functions
         heaviside.vector()[dofmap.cell_dofs(cell.index())] = 1
 	#project the characteristic function cf into our space V 
 	# ie - this is the heaviside function	
-Dx=3
-Dy=3
-Dz=3
+Dx=Constant(9.2e-7*100)
+Dy=Constant(9.2e-7*100)
+Dz=Constant(2.1e-6*100)
 # Define variational problem
 D = sym(as_tensor([[Dx,  0,  0],
 	           [0,  Dy,  0],
 	           [0,   0, Dz]])) 
 # dispersion coefficient matrix
-uA = Function(V)  # Note: not TrialFunction!
+uA = TrialFunction(V)
+u = Function(V)  # Note: not TrialFunction!
 uB = interpolate(u_0,V)
 v = TestFunction(V)
 
-tF=10
-w=1
-K=10*heaviside #degradation only happening inside the subdomain
-F = dot(D*grad(uA), grad(v))*dx + v*(uA-uB)/dt*dx - v*w*grad(uA)[2]*dx + K*uA*v*dx
+tF=50
+w=Constant(0.004*100)
+K=2.304e-4*100*heaviside #degradation only happening inside the subdomain
+A = inner(D*grad(uA), grad(v))*dt*dx + v*uA*dx - v*w*dt*grad(uA)[2]*dx + dt*K*uA*v*dx
+L=v*uB*dx
 counter = 1
-vtkfile = File('diffusion3d/solution.pvd')
+vtkfile = File('diffusion3d_2/solution.pvd')
 
 while t<=tF:
 	t+=dt
-	solve(F==0,uA)
-	uA.rename('uA','uA')
-	vtkfile << uA, counter
-	uB.assign(uA)
+	solve(A==L,u)
+	u.rename('u','u')
+	if counter % 1 == 0:
+		vtkfile << u, counter
+		print('\nTimestep: '+str(counter)+'\n')
+	uB.assign(u)
 	counter+=1
 
 
